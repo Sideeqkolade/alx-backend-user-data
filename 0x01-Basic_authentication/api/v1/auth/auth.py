9772f9ac11e9e598bd815b36422f2bd21499f0fe
Sideeqkolade <sideeqkolade@gmail.com>
2023-09-05 01:32:04 +0100

    task 4 completed

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,26 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if a path requires authentication
         """
-        return False
+        # check if path is None
+        if path is None:
+            return True
+
+        # check if excluded_path is None or empty
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # check if path is in the list of excluded_paths
+        if path in excluded_paths:
+            return False
+
+        normal_path = path.rstrip('/')  # remove a trailing slash if it exists
+        normal_exclude_paths = [p.rstrip('/') for p in excluded_paths]
+
+        # Check if normal_path is in normal_exclude_paths
+        if normal_path in normal_exclude_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
