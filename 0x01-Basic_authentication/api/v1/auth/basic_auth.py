d53a523c05cfb34f86099fc1e7059463ac5fc6ee
Sideeqkolade <sideeqkolade@gmail.com>
2023-09-05 04:13:58 +0100

    task 6 completed

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API."""
+from .auth import Auth
+# from models.user import User
+
+
+class BasicAuth(Auth):
+    """BAsic Authentication Class"""
