171d9d127345373f72c4b5b470abcac32537d7c3
Sideeqkolade <sideeqkolade@gmail.com>
2023-09-05 20:53:35 +0100

    task 8 completed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic authentication module for the API."""
 from .auth import Auth
+import base64
 # from models.user import User
 
 
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
                 if len(split_string) == 2:
                     return split_string[1]
             return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """ Decodes a base64-encoded authorization header.
+        """
+        if base64_authorization_header is not None and isinstance(
+                base64_authorization_header, str):
+            try:
+                decoded_data = base64.b64decode(base64_authorization_header)
+                return decoded_data.decode('utf-8')
+            except (base64.binascii.Error, UnicodeDecodeError):
+                return None
