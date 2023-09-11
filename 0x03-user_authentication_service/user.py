ae55dd28f937d3df1085263a6fc430093b665933
Sideeqkolade <sideeqkolade@gmail.com>
2023-09-11 22:37:08 +0100

    task 0 completed

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" A module for user model
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ A user model """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(256), nullable=False)
+    hashed_password = Column(String(256), nullable=False)
+    session_id = Column(String(256), nullable=True)
+    reset_token = Column(String(256), nullable=True)
